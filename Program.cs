2a5e9c404c74d4c024dae77060e514e3be60fceb
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-04-22 14:31:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrom_Number");
+            int n, temp, sum = 0, rev;
+            Console.WriteLine("Enter the number : ");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                rev = n % 10;
+                sum = (sum * 10) + rev;
+                n = n / 10;
+            }
+            if(temp == sum)
+            {
+                Console.WriteLine("Number is Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Palindrom");
+            }
+            Console.ReadLine();
+        }
+    }
+}
